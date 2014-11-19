ec4bff36862d5f6582ae53b8b3e33878f626c416
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-11-19 18:46:25 +0100

    Create a model in PelisCtrl

--- scripts/app.js
+++ scripts/app.js
@@ -4,14 +4,14 @@ angular.module("pelis").config(["$routeProvider", function($routeProvider)
 {
 	$routeProvider.when(
 		"/pelis", {
-			controllerUrl: "PelisCtrl", // Carga el controlador por nombre, mira en la pila de controladores
+			controller: "PelisCtrl", // Carga el controlador por nombre, mira en la pila de controladores
 			templateUrl: "views/Pelis.html" // Busca la ruta en la raíz, por eso ponemos la ruta entera
 		}
 	);
 
 	$routeProvider.when(
 		"/series", {
-			controllerUrl: "SeriesCtrl",
+			controller: "SeriesCtrl",
 			templateUrl: "views/Series.html"
 		}
 	);
--- scripts/controllers/PelisCtrl.js
+++ scripts/controllers/PelisCtrl.js
@@ -1,5 +1,13 @@
 angular
 	.module("pelis")
 	.controller("PelisCtrl", function($scope){
+		$scope.movie = {
+			title: "The Matrix",
+			year: 1999
+		};
 
+		var serie = {
+			title: "Breaking Bad",
+			year: "A saber"
+		};
 	})
