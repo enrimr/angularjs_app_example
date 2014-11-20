16bc157ec3c5136a6259ee29f7a5e0f618d7a850
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-11-20 15:50:52 +0100

    Call to movie api

--- scripts/controllers/PelisCtrl.js
+++ scripts/controllers/PelisCtrl.js
@@ -1,6 +1,21 @@
 angular
 	.module("pelis")
-	.controller("PelisCtrl", function($scope){
+	.controller("PelisCtrl", function($scope, $http){
+
+		//var api =  "http://tmdbapitest.appspot.com/api/";
+
+		var api = "https://api.themoviedb.org/3/";
+		var api_key = "826b523c417cbb888744b13031d846c2";
+		var language = "es";
+		$http.get(api + "movie/upcoming?api_key="+api_key+"&language="+language).then(
+			function(datos){
+				alert("Todo fue bien");
+			},
+
+			function(){
+				alert("Hubo un error");
+			}
+		);
 
 		$scope.lanzarBroadcast = function(){
 			$scope.$broadcast("MiEvento", "Hola!!"); // Creamos un evento del scope para propagar hacia abajo
@@ -8,5 +23,5 @@ angular
 
 		$scope.$on("MiOtroEvento", function(event, datos){
 			alert(datos);
-		})
+		});
 	});
