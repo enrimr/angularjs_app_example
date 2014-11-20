212360933e208619a2add422142c2eeee355a53b
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-11-20 15:16:53 +0100

    Add emit event

--- scripts/controllers/PelisCtrl.js
+++ scripts/controllers/PelisCtrl.js
@@ -5,4 +5,8 @@ angular
 		$scope.lanzarBroadcast = function(){
 			$scope.$broadcast("MiEvento", "Hola!!"); // Creamos un evento del scope para propagar hacia abajo
 		};
+
+		$scope.$on("MiOtroEvento", function(event, datos){
+			alert(datos);
+		})
 	});
--- scripts/controllers/PelisProximamenteCtrl.js
+++ scripts/controllers/PelisProximamenteCtrl.js
@@ -4,4 +4,8 @@ angular
 		$scope.$on("MiEvento", function(event, datos){
 			alert(datos);
 		});
+
+		$scope.lanzarEmit = function(){
+			$scope.$emit("MiOtroEvento", "Adios!!"); // Creamos un evento del scope para propagar hacia arriba
+		};
 	});
