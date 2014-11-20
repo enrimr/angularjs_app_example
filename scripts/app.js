0f4a2d986e0008c43140654158c63be7b7c89bfb
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-11-20 12:54:47 +0100

    Add angular-route-segment vendor and add app-view-segment='0'

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,18 @@
-angular.module("pelis", ["ngRoute"]);
+angular.module("pelis", ["ngRoute", "view-segment", "route-segment"]);
 
-angular.module("pelis").config(["$routeProvider", function($routeProvider)
+angular.module("pelis").config(["$routeSegmentProvider", function($routeSegmentProvider)
 {
-	$routeProvider.when(
-		"/pelis", {
-			controller: "PelisCtrl", // Carga el controlador por nombre, mira en la pila de controladores
-			templateUrl: "views/Pelis.html" // Busca la ruta en la raíz, por eso ponemos la ruta entera
-		}
-	);
+	$routeSegmentProvider.when("/pelis", "pelis"); // "ruta", "identificadorDelSegmento"
+	$routeSegmentProvider.when("/series", "series");
 
-	$routeProvider.when(
-		"/series", {
-			controller: "SeriesCtrl",
-			templateUrl: "views/Series.html"
-		}
-	);
+	$routeSegmentProvider.segment("pelis", { // El primer parámetro es el identificador del segmento que queramos
+		controller: "PelisCtrl", // Carga el controlador por nombre, mira en la pila de controladores
+		templateUrl: "views/Pelis.html" // Busca la ruta en la raíz, por eso ponemos la ruta entera
+	});
+
+	$routeSegmentProvider.segment("series", {
+		controller: "SeriesCtrl",
+		templateUrl: "views/Series.html"
+	});
+
 }])
