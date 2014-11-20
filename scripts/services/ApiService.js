ce81c1daf7e6edfa0b4d578cbc79bcecb2ec8b6f
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-11-20 17:38:54 +0100

    Fix bug when movie doesn't have got a picture

--- scripts/services/ApiService.js
+++ scripts/services/ApiService.js
@@ -1,7 +1,9 @@
 angular.module("pelis").service("ApiService", function() {
 
 	this.rutaImagen = function(path, size){
-		return "http://image.tmdb.org/t/p/w" + size + path;
+		return path !== null
+			? "http://image.tmdb.org/t/p/w" + size + path
+			: null;
 	};
-	
+
 });
