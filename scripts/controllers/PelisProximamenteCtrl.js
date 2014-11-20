ec6cad1ef66bb1e7de7338fcbefc1f11f0f32b5c
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-11-20 18:00:30 +0100

    Create service to make get petitions to api

--- scripts/controllers/PelisProximamenteCtrl.js
+++ scripts/controllers/PelisProximamenteCtrl.js
@@ -9,10 +9,11 @@ angular
 			$scope.$emit("MiOtroEvento", "Adios!!"); // Creamos un evento del scope para propagar hacia arriba
 		};
 
-		var api = "https://api.themoviedb.org/3/";
-		var api_key = "826b523c417cbb888744b13031d846c2";
-		var language = "es";
-		$http.get(api + "movie/upcoming?api_key="+api_key+"&language="+language).then(
+		$scope.obtenerRutaImagen = function(path){
+			return ApiService.rutaImagen(path, 45);
+		};
+
+		ApiService.obtenerDatosApi("movie/upcoming").then(
 			// .get funciona con promesas, no con callbacks.
 			// La primera función se ejecuta si todo ha ido bien
 			function(datos){
@@ -24,7 +25,4 @@ angular
 			}
 		);
 
-		$scope.obtenerRutaImagen = function(path){
-			return ApiService.rutaImagen(path, 45);
-		};
 	});
--- scripts/services/ApiService.js
+++ scripts/services/ApiService.js
@@ -1,9 +1,16 @@
-angular.module("pelis").service("ApiService", function() {
+angular.module("pelis").service("ApiService", function($http) {
+	var api = "https://api.themoviedb.org/3/";
+	var api_key = "826b523c417cbb888744b13031d846c2";
+	var language = "es";
 
 	this.rutaImagen = function(path, size){
 		return path !== null
 			? "http://image.tmdb.org/t/p/w" + size + path
 			: null;
+	};
+
+	this.obtenerDatosApi = function(path){
+		return $http.get(api + path + "?api_key=" + api_key + "&language=" + language);
 	};
 
 });
